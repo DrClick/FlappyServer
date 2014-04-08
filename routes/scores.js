8e78143449ac82c5202a2e4bf1fde287b6ad48cb
drclick <drclick@mac.com>
2014-04-07 17:24:28 -0700

    returns to 30 scores and stores scores

--- routes/scores.js
+++ routes/scores.js
@@ -4,23 +4,47 @@ var mongoUri = "mongodb://admin:0okm9ijN@ds030607.mongolab.com:30607/flappybird"
 
 
 exports.getTopScores = function(req, res) {
-	mongo.Db.connect(mongoUri, function (err, db) {
-	  db.collection('highScores', function(er, collection) {
-	    //get the records
-	  });
-	});
-    res.send([{name:'wine1'}, {name:'wine2'}, {name:'wine3'}]);
-};
 
-exports.postScore = function(data){
+	//return the top 30 scores for now
 	mongo.Db.connect(mongoUri, function (err, db) {
-	  db.collection('highScores', function(er, collection) {
-	    collection.insert(
-	    	{
-	    		"name": "Tom Watson", 
-	    		"score" : 234, 
-	    		"date": Date.now() 
-	    	}, {safe: true}, function(er,rs) {});
-	  });
-	});
+	 	var highScoresCollection = db.collection('highScores');
+		
+		var scores = highScoresCollection
+			.find()
+			.sort({score:1})
+			.limit(30)
+			.toArray(function(err, items){
+				console.log(items);
+				res.send(items);
+			});
+	});//end connect
+};//end method
+
+exports.postScore = function(req, res){
+	console.log(req.body);
+	var data = JSON.parse(req.body["data"]);
+	mongo.Db.connect(mongoUri, function (err, db) {
+
+		var highScoresCollection = db.collection('highScores');
+
+		//check to see if score exists
+		highScoresCollection.find({
+			userId: data.id,
+			score: data.score
+		}).toArray(function(err, items){
+			if(!items.length){
+				highScoresCollection.insert(
+		    	{
+		    		"name": data.name, 
+		    		"score" : data.score, 
+		    		"date": Date.now(),
+		    		"userId": data.id
+		    	}, {safe: true}, function(er,rs) {});
+			}//end if does not exist
+		});//end to array
+	});//end connection
+
+	res.send("Nice Score!");
 }
+
+
--- web.js
+++ web.js
@@ -5,11 +5,13 @@ var scores = require("./routes/scores");
 
 var app = express();
 
-
+console.log("start");
 app.configure(function () {
     app.use(logfmt.requestLogger());
     app.use(express.bodyParser());
+    app.use(_allowCrossDomain);
 });
+console.log("configured");
 
 
 //define API
@@ -18,13 +20,23 @@ app.get('/', function(req, res) {
 });
 
 app.get("/scores", scores.getTopScores);
-app.post("/scores" scores.postScore);
+app.post("/scores", scores.postScore);
 
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+function _allowCrossDomain(req, res, next){
+	res.header('Access-Control-Allow-Origin', "*");
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+ 
+	next();
+}
+
+
 
 
 
